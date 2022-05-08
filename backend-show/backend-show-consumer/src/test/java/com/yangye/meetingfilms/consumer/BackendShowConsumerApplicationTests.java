c7db0db6377c1c6f23b3ace10082d435cb958a37
yangye <956830514@qq.com>
2022-05-08 20:55:51 +0800

    完善项目

---
+++ backend-show/backend-show-consumer/src/test/java/com/yangye/meetingfilms/consumer/BackendShowConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangye.meetingfilms.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendShowConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ backend-show/backend-show-provider/src/main/java/com/yangye/meetingfilms/provider/BackendShowProviderApplication.java
@@ -0,0 +1,13 @@
+package com.yangye.meetingfilms.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendShowProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendShowProviderApplication.class, args);
+    }
+
+}
