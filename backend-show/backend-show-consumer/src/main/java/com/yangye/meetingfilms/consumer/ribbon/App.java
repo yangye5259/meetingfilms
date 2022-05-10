32cae17bd1c40ef130e653ececf1496f5d416376
yangye <956830514@qq.com>
2022-05-10 14:06:17 +0800

    完善项目

---
+++ backend-show/backend-show-consumer/src/main/java/com/yangye/meetingfilms/consumer/ribbon/App.java
@@ -0,0 +1,12 @@
+package com.yangye.meetingfilms.consumer.ribbon;
+
+/**
+ * @Author: yangye
+ * @Date: 2022/5/10 11:48
+ */
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
